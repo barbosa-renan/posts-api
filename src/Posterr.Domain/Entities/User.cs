ae66d24d2c2fec0cb9480b986f9de53d8b720d4a
Renan Barbosa <renanvinicius.quadros@gmail.com>
2022-04-21 09:37:17 -0300

    Creation of user entity

---
+++ src/Posterr.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Posterr.Domain.Entities
+{
+    public sealed class User : BaseEntity<Guid>
+    {
+        public string UserName { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        //EF Relations
+        public ICollection<User> Followers { get; set; }
+        public ICollection<User> Following { get; set; }
+    }
+}
